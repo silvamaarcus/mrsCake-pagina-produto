becbd2993ed0d898b63234107ba215f7f6a163a0
Marcus Silva <devsilvamarcus@gmail.com>
2023-04-02 14:52:11 -0300

    regra adicionada  para não aplicar zoom na imagem, se a tela for menor 800px.

--- src/js/zoomImagem.js
+++ src/js/zoomImagem.js
@@ -3,21 +3,16 @@ export default function zoomImagem() {
   const img = document.querySelector('#principal-img');
   const foco = document.querySelector('.foco');
 
-  let tamanhoTela = window.innerWidth;
-
   container.addEventListener('mouseover', () => {
-    img.style.transform = 'scale(1.5)';
-    foco.style.display = 'block';
+    if (window.innerWidth >= 800) {
+      img.style.transform = 'scale(1.5)';
+      foco.style.display = 'block';
+    };
   });
   container.addEventListener('mouseout', () => {
     img.style.transform = 'scale(1)';
     foco.style.display = 'none';
   });
 
-  // if (tamanhoTela < 800) {
-  //   container.addEventListener('mouseover', () => {
-  //     img.style.transform = 'none';
-  //     foco.style.display = 'none';
-  //   });
-  // }
+
 }
